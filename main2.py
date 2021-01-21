4715140e52c0dd1cefc3a284e392121ed0e7404f
malcolmsfraser <68970928+malcolmsfraser@users.noreply.github.com>
2021-01-20 17:02:13 -0800

    Update main2.py

--- main2.py
+++ main2.py
@@ -35,6 +35,14 @@ def bob():
     val = {"Bob" : "You're not Bob, I AAM BOB!!"}
     return jsonify(val)
 
+@app.errorhandler(500)
+def server_error(e):
+    logging.exception('An error occurred during a request.')
+    return """
+    An internal error occurred: <pre>{}</pre>
+    See logs for full stacktrace.
+    """.format(e), 500
+
 
 #@app.route('/wikipedia/<company>')
 #def wikipedia_route(company):
